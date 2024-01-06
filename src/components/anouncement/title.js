de402c9d6803e38a3843ed798e999c315f4768a7
Jayavenkate <jayajaikithuja@gmail.com>
2024-01-07 00:48:03 +0300

    ui

--- src/components/anouncement/title.js
+++ src/components/anouncement/title.js
@@ -1,26 +1,9 @@
-import { Button } from "@mui/material";
 import styles from "./title.module.css";
 export default function Announcement() {
   return (
     <div className={styles.divcon}>
-      <Button
-        sx={{
-          background: "rgb(228, 227, 227)",
-          textTransform: "none",
-          color: "black",
-          minWidth: "98px",
-          height: "16px",
+      <button className={styles.button}>Announcement</button>
 
-          fontWeight: 500,
-          fontSize: "12px",
-          lineHeight: "16px",
-          padding: "10px",
-          borderRadius: "10px",
-        }}
-      >
-        Announcement
-      </Button>
-      {/* <Button className={styles.button}>Announcement</Button> */}
       <p className={styles.pelement}>
         We are happy to announce that we raise $2 Million in Seed Funding
       </p>
